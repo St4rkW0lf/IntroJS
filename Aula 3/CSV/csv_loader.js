8e798d669e74977e9c78bb2681c04f7041351ac9
Wh1t3W0lf <55291037+St4rkW0lf@users.noreply.github.com>
2020-10-21 22:47:10 -0300

    Add files via upload

---
+++ Aula 3/CSV/csv_loader.js
@@ -0,0 +1,12 @@
+export function csv_loader(text, column_separator = ";", line_separator = "\n"){
+    return text
+        .trim()
+        .split(line_separator)
+        .map(
+            line => line
+                .trim()
+                .split(column_separator)
+                .map(cell => cell.trim())
+        )
+
+}
---
+++ Aula 3/CSV/file.js
@@ -0,0 +1,32 @@
+/*
+import { csv_loader } from './csv_loader.js';
+
+const csv = await Deno.readTextFile('./municipios.csv');
+
+const tabela = csv_loader(csv);
+
+console.log(tabela);
+*/
+
+
+
+const file = await Deno.readTextFile('./file.json');
+
+const json = JSON.parse(file);
+
+console.log(json[0].interesses[1]);
+
+
+
+const Carlos = {
+    nome:"Carlos",
+    idade: 28,
+    brasileiro: true
+
+}
+
+console.log(carlos);
+
+const json_final = JSON.stringify(json, null, 2);
+
+await Deno.writeTextFile('./carlos.json', json_final);
